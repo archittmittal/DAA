f9d3f4d16065de3e9ff27a47e93247dfa0939228
Archit Mittal <work.architmittal@gmail.com>
2026-02-12 13:50:50 +0530

    Added quick sort into the class

--- DivideandConquer.java
+++ DivideandConquer.java
@@ -1,4 +1,4 @@
-public class mergesort {
+public class DivideandConquer {
 
     public static void Printarr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
@@ -53,9 +53,41 @@ public class mergesort {
         }
     }
 
+    public static void quickSort(int arr[],int si,int ei){
+        if (si >= ei) {
+            return;
+        }
+
+        int p_index = partition(arr,si,ei); // pivot ki value dega
+        quickSort(arr,si,p_index-1); // left
+        quickSort(arr,p_index+1,ei); // right
+    }
+
+    public static int partition(int arr[],int si,int ei){
+        int pivot = arr[ei];
+        int i = si-1; // makes place for elemnts smaller than pivot
+        for(int j =0 ; j<ei;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp = arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+                int temp = pivot;
+                arr[ei]=arr[i]; // main 
+                arr[i]=temp;
+                return i;
+
+
+    }
+
     public static void main(String[] args) {
         int arr[] = {3, 6, 9, 2, 7, 5};
         merge(arr, 0, arr.length - 1);
+        Printarr(arr);
+        quickSort(arr, 0, arr.length-1);
         Printarr(arr);
     }
 }
